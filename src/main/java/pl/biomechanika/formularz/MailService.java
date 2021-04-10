e8c4c59349f385fde696d6ecc40c3d563d0d3d48
Jakub Słowiński <jakubslowinski81@gmail.com>
2021-04-10 15:39:58 +0200

    pierwsze podejscie

--- src/main/java/pl/biomechanika/formularz/MailService.java
+++ src/main/java/pl/biomechanika/formularz/MailService.java
@@ -1,6 +1,5 @@
 package pl.biomechanika.formularz;
 
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
